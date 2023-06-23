aa6af395f1044d8654cdc375d8f5d43b0dbf9a13
Giloc Delgado <giloc.delgado@udea.edu.co>
2023-06-22 20:57:12 -0500

    Primer commit de la rama Giloc

---
+++ Assets/Scripts/Maths/Ejemplo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejemplo : MonoBehaviour
+{
+    public int variable = 0;
+
+    private void HacerAlgo()
+    {
+        //Haga algo
+    }
+}
